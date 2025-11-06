480023c40567ab11a48f6a0e2ceb416a4ccbe1de
Ankit Mishra <ankitmishra16127@gmail.com>
2025-11-07 01:42:00 +0530

    show

--- tballot/contractConfig.ts
+++ tballot/contractConfig.ts
@@ -1,4 +1,4 @@
-import abi from '../Contracts/artifacts/contracts/TrustBallot.sol/TrustBallot.json'
+import abi from './trustBallot.json'
 
 export const CONTRACT_ADDRESS = "0x31Beb054e1B326853c9AbD8ed2dd71FD0D0B77cA";
 
