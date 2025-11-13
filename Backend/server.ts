34282c3037d10eed0569d71d91d5934c963bd08e
Ankit Mishra <ankitmishra16127@gmail.com>
2025-11-13 20:32:37 +0530

    changes in cors

--- Backend/server.ts
+++ Backend/server.ts
@@ -17,11 +17,7 @@ const JWT_SECRET =process.env.JWT_SECRET;
 
 
 app.use(cors({
-  origin: [
-    "http://localhost:5173",
-    "http://localhost:5174",
-    "https://trust-ballot.vercel.app"
-  ],
+  origin: "https://trust-ballot.vercel.app",
   credentials: true
 }));
 
